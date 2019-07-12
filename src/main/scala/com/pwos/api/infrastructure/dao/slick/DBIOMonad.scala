8f85b5db00ac232a395130c27b2859498e0f7a4f
Piotr <ptr.wos@gmail.com>
2019-07-13 01:41:35 +0200

    Fix whitespace

--- src/main/scala/com/pwos/api/infrastructure/dao/slick/DBIOMonad.scala
+++ src/main/scala/com/pwos/api/infrastructure/dao/slick/DBIOMonad.scala
@@ -8,7 +8,7 @@ import scala.concurrent.ExecutionContext
 
 trait DBIOMonad {
   implicit def DBIOMonad(implicit executionContext: ExecutionContext): Monad[DBIO] = new Monad[DBIO] {
-    override def pure[A](x: A): DBIO[A]                                 = DBIO.successful(x)
+    override def pure[A](x: A): DBIO[A] = DBIO.successful(x)
     override def flatMap[A, B](fa: DBIO[A])(f: A => DBIO[B]): DBIO[B] = fa flatMap f
     override def tailRecM[A, B](a: A)(f: A => DBIO[Either[A, B]]): DBIO[B] = f(a) flatMap {
       case Left(a1) => tailRecM(a1)(f)
