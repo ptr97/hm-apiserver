7e0bdc0ab75a35ec08ec5c6e46f819d59f3968df
Piotr <ptr.wos@gmail.com>
2019-07-24 20:22:05 +0200

    First version of PlaceController before refactor

--- src/main/scala/com/pwos/api/infrastructure/http/PlaceController.scala
+++ src/main/scala/com/pwos/api/infrastructure/http/PlaceController.scala
@@ -2,17 +2,21 @@ package com.pwos.api.infrastructure.http
 
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import com.pwos.api.domain.PlaceAlreadyExistsError
 import com.pwos.api.domain.PlaceNotFoundError
-import com.pwos.api.domain.places.{Place, PlaceService}
+import com.pwos.api.domain.places.Place
+import com.pwos.api.domain.places.PlaceService
 import com.pwos.api.infrastructure.dao.slick.DBIOMonad._
 import com.pwos.api.infrastructure.implicits._
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
+import io.circe.Json
 import io.circe.generic.auto._
 import io.circe.syntax._
 import slick.dbio.DBIO
 import slick.jdbc.MySQLProfile.api._
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
 
 
 class PlaceController(placeService: PlaceService[DBIO])(implicit ec: ExecutionContext, database: Database) {
@@ -25,22 +29,41 @@ class PlaceController(placeService: PlaceService[DBIO])(implicit ec: ExecutionCo
   def addPlace: Route = path(PLACES) {
     post {
       entity(as[Place]) { place: Place =>
-        complete(Place("Place", 10, 10, 10).asJson)
+        val createdPlaceDBIO: DBIO[Either[PlaceAlreadyExistsError, Place]] = placeService.create(place).value
+        val createdPlaceFuture: Future[Either[PlaceAlreadyExistsError, Place]] = createdPlaceDBIO.unsafeRun
+        val response: Future[Json] = createdPlaceFuture.map {
+          case Right(createdPlace) => {
+            val t = createdPlace.asJson
+            println(t)
+            t
+          }
+          case Left(placeAlreadyExistsError) => placeAlreadyExistsError.asJson
+        }
+
+        complete(response)
       }
     }
   }
 
-  def getPlace: Route = path(PLACES / LongNumber) { placeId =>
+  def getPlace: Route = path(PLACES / LongNumber) { placeId: Long =>
     get {
       val placeDBIO: DBIO[Either[PlaceNotFoundError.type, Place]] = placeService.get(placeId).value
       val placeFuture: Future[Either[PlaceNotFoundError.type, Place]] = placeDBIO.unsafeRun
 
-      val res: Future[String] = placeFuture.map {
-        case Right(place) => "hura"
-        case Left(placeNotFoundError) => "ojej"
+      val response = placeFuture.map {
+        case Right(place) => {
+          val t = place.asJson
+          println(t)
+          t
+        }
+        case Left(placeNotFoundError) => {
+          val t = placeNotFoundError.asJson
+          println(t)
+          t
+        }
       }
 
-      complete(res)
+      complete(response)
     }
   }
 
