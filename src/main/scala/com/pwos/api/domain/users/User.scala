6325c57776469326e8fc9b3a754a68de25624cde
Piotr <ptr.wos@gmail.com>
2019-08-03 20:59:41 +0200

    Add user models v1

--- src/main/scala/com/pwos/api/domain/users/User.scala
+++ src/main/scala/com/pwos/api/domain/users/User.scala
@@ -1,7 +1,34 @@
 package com.pwos.api.domain.users
 
+
 case class User(
+                 id: Long,
                  userName: String,
                  email: String,
-                 id: Option[Long] = None
-               )
+                 password: User.Password,
+                 role: UserRole.Value = UserRole.User,
+                 banned: Boolean = false,
+                 deleted: Boolean = false
+               ) {
+  def toView: UserView = UserView(id, userName, email)
+}
+
+object User {
+  type Password = String
+}
+
+
+object UserModels {
+
+  case class RegistrationModel(userName: String, email: String, password: String, passwordCheck: String)
+
+  case class LoginModel(userNameOrEmail: String, password: String)
+
+  case class UpdateUserCredentialsModel(userName: Option[String], email: Option[String])
+
+  case class UpdatePasswordModel(oldPassword: String, newPassword: String, newPasswordCheck: String)
+}
+
+case class UserInfo(id: Long, userName: String, email: String, role: UserRole.Value, banned: Boolean, deleted: Boolean)
+
+case class UserView(id: Long, userName: String, email: String)
---
+++ src/main/scala/com/pwos/api/domain/users/UserRole.scala
@@ -0,0 +1,7 @@
+package com.pwos.api.domain.users
+
+
+object UserRole extends Enumeration {
+  val User: Value = Value("user")
+  val Admin: Value = Value("admin")
+}
