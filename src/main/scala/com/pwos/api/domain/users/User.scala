bde067d4fda2db0df17f01adaeea9bca68582734
Piotr <ptr.wos@gmail.com>
2019-08-04 00:58:48 +0200

    User models v2

--- src/main/scala/com/pwos/api/domain/users/User.scala
+++ src/main/scala/com/pwos/api/domain/users/User.scala
@@ -2,15 +2,17 @@ package com.pwos.api.domain.users
 
 
 case class User(
-                 id: Long,
                  userName: String,
                  email: String,
                  password: User.Password,
                  role: UserRole.Value = UserRole.User,
                  banned: Boolean = false,
-                 deleted: Boolean = false
+                 deleted: Boolean = false,
+                 id: Option[Long]
                ) {
-  def toView: UserView = UserView(id, userName, email)
+  def toView: Option[UserView] = id.map { id => UserView(id, userName, email) }
+
+  def buildUserInfo: Option[UserInfo] = id.map { id => UserInfo(id, userName, email, role, banned, deleted) }
 }
 
 object User {
@@ -20,13 +22,15 @@ object User {
 
 object UserModels {
 
-  case class RegistrationModel(userName: String, email: String, password: String, passwordCheck: String)
+  case class CreateUserModel(userName: String, email: String, password: String, passwordCheck: String)
 
   case class LoginModel(userNameOrEmail: String, password: String)
 
   case class UpdateUserCredentialsModel(userName: Option[String], email: Option[String])
 
-  case class UpdatePasswordModel(oldPassword: String, newPassword: String, newPasswordCheck: String)
+  case class UpdateUserStatusModel(banned: Boolean)
+
+  case class ChangePasswordModel(oldPassword: String, newPassword: String, newPasswordCheck: String)
 }
 
 case class UserInfo(id: Long, userName: String, email: String, role: UserRole.Value, banned: Boolean, deleted: Boolean)
