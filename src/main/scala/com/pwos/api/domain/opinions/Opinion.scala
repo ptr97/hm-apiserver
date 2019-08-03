57674a73f5fec972542a959336b42f275c0c5543
Piotr <ptr.wos@gmail.com>
2019-08-04 00:35:15 +0200

    Opinion models v1

--- src/main/scala/com/pwos/api/domain/opinions/Opinion.scala
+++ src/main/scala/com/pwos/api/domain/opinions/Opinion.scala
@@ -1,5 +1,38 @@
 package com.pwos.api.domain.opinions
 
-class Opinion {
+import akka.http.scaladsl.model.DateTime
+import com.pwos.api.domain.tags.Tag
 
+
+case class Opinion(
+                  uuid: String,
+                  placeId: Long,
+                  authorId: Long,
+                  body: Option[String],
+                  tags: List[Tag],
+                  likes: List[String],
+                  referenceDate: DateTime,
+                  creationDate: DateTime = DateTime.now,
+                  lastModified: DateTime,
+                  blocked: Boolean = false,
+                  deleted: Boolean = false,
+                  id: Option[Long]
+                  )
+
+object OpinionModels {
+  case class CreateOpinionModel(
+                            placeId: Long,
+                            authorId: Long,
+                            body: Option[String],
+                            tags: List[Tag],
+                            creationDate: DateTime = DateTime.now,
+                            referenceDate: Option[DateTime]
+                          )
+
+  case class UpdateOpinionModel(
+                                 uuid: String,
+                                 body: Option[String],
+                                 tags: Option[List[Tag]],
+                                 referenceDate: Option[DateTime]
+                               )
 }
