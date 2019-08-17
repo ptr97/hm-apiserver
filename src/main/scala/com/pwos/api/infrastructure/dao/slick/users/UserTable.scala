3e7d4b2372d90f5fd0a1688245b82737d154c2e0
Piotr <ptr.wos@gmail.com>
2019-08-17 20:56:45 +0200

    Add User table

---
+++ src/main/scala/com/pwos/api/infrastructure/dao/slick/users/UserTable.scala
@@ -0,0 +1,33 @@
+package com.pwos.api.infrastructure.dao.slick.users
+
+import com.pwos.api.domain.users.User
+import com.pwos.api.domain.users.UserRole
+import slick.jdbc.MySQLProfile.api._
+import slick.lifted.ProvenShape
+
+import scala.language.postfixOps
+
+
+class UserTable(tag: Tag) extends Table[User](tag, "USER") {
+
+  implicit val enumMapping = MappedColumnType.base[UserRole.Value, String](_.toString, s => UserRole.withName(s))
+
+  def id: Rep[Long]               = column[Long]("ID", O.PrimaryKey, O.AutoInc)
+  def username: Rep[String]       = column[String]("USERNAME")
+  def email: Rep[String]          = column[String]("EMAIL")
+  def password: Rep[String]       = column[String]("PASSWORD")
+  def role: Rep[UserRole.Value]   = column[UserRole.Value]("ROLE")
+  def banned: Rep[Boolean]        = column[Boolean]("BANNED")
+  def deleted: Rep[Boolean]       = column[Boolean]("DELETED")
+
+
+  override def * : ProvenShape[User] = (
+    username,
+    email,
+    password,
+    role,
+    banned,
+    deleted,
+    id.?
+  ) <> (User.apply _ tupled, User.unapply)
+}
