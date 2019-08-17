316fe2c1062b9f3396d23d5c0b04d8198897f9f5
Piotr <ptr.wos@gmail.com>
2019-08-17 20:52:23 +0200

    Rename Place ID column

--- src/main/scala/com/pwos/api/infrastructure/dao/slick/places/PlaceTable.scala
+++ src/main/scala/com/pwos/api/infrastructure/dao/slick/places/PlaceTable.scala
@@ -7,7 +7,7 @@ import slick.lifted.ProvenShape
 
 final class PlaceTable(tag: Tag) extends Table[Place](tag, "PLACE") {
 
-  def id: Rep[Long]          = column[Long]("id", O.PrimaryKey, O.AutoInc)
+  def id: Rep[Long]          = column[Long]("ID", O.PrimaryKey, O.AutoInc)
   def name: Rep[String]      = column[String]("NAME")
   def latitude: Rep[Double]  = column[Double]("LATITUDE")
   def longitude: Rep[Double] = column[Double]("LONGITUDE")
