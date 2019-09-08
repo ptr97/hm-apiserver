d8035969c7cda96f8f37a0b0bb7efef021947936
Piotr Wos <ptr.wos@gmail.com>
2019-09-08 14:16:11 +0200

    Prepare test classes

---
+++ src/test/scala/com/pwos/api/domain/authentication/AuthServiceSpec.scala
@@ -0,0 +1,9 @@
+package com.pwos.api.domain.authentication
+
+import org.scalatest.FunSpec
+import org.scalatest.Matchers
+
+
+class AuthServiceSpec extends FunSpec with Matchers {
+
+}
---
+++ src/test/scala/com/pwos/api/domain/opinions/OpinionServiceSpec.scala
@@ -0,0 +1,9 @@
+package com.pwos.api.domain.opinions
+
+import org.scalatest.FunSpec
+import org.scalatest.Matchers
+
+
+class OpinionServiceSpec extends FunSpec with Matchers {
+
+}
