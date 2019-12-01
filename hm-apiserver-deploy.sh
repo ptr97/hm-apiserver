c3cd248d56c359724c513e281e92f4af996cc86d
Piotr Wos <ptr.wos@gmail.com>
2019-12-01 14:54:49 +0100

    Update deploy scripts.

--- hm-apiserver-deploy.sh
+++ hm-apiserver-deploy.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-SOURCE_CODE_DIR="./hm-apiserver"
+# SOURCE_CODE_DIR="./hm-apiserver"
+# cd $SOURCE_CODE_DIR || return 127
+
 JAR_NAME="hm-apiserver-0.0.1-SNAPSHOT.jar"
 LOG_FILE="output.log"
-
-cd $SOURCE_CODE_DIR || return 127
+JAR_DEST="./hm-apiserver/target/scala-2.12/${JAR_NAME}"
+WORK_DIR="./run_dir/"
 
 export SBT_OPTS="-XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=2G -Xmx2G"
 /usr/local/sbt/bin/sbt -Dsbt.log.noformat=true assembly
-
-JAR_DEST="./hm-apiserver/target/scala-2.12/${JAR_NAME}"
-WORK_DIR="./run_dir/"
 
 cd ../
 cp $JAR_DEST $WORK_DIR
--- hm-apiserver-test.sh
+++ hm-apiserver-test.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-SOURCE_CODE_DIR="./hm-apiserver"
-
-cd $SOURCE_CODE_DIR || return 127
+# SOURCE_CODE_DIR="./hm-apiserver"
+# cd $SOURCE_CODE_DIR || return 127
 
 export SBT_OPTS="-XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=2G -Xmx2G"
 /usr/local/sbt/bin/sbt -Dsbt.log.noformat=true test
